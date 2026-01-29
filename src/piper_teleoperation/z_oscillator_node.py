7765783b9a7d69926fa39778ed630bf37de6115c
Alan Ding <dkchuah@hotmail.com>
2026-01-29 14:50:44 +0100

    Integrating hand detection and hand shaking

--- src/piper_teleoperation/z_oscillator_node.py
+++ src/piper_teleoperation/z_oscillator_node.py
@@ -3,7 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Point
-
+from std_msgs.msg import Bool
 
 class PiperZOscillator(Node):
     def __init__(self):
@@ -27,8 +27,28 @@ class PiperZOscillator(Node):
 
         self.get_logger().info("🚀 Piper Z Oscillator started")
         self.get_logger().info("⬆️⬇️  Oscillating ±5 cm in Z")
+        
+        # Hand Detection
+        self.hand_present = False # Added for Hand Detection
+        self.last_true_time = 0.0 # Added for Hand Detection
+        self.hold_time = 0.5 # Added for Hand Detection
+        self.hand_sub = self.create_subscription(Bool, '/hand/semantic_detected', self.hand_callback, 10) # Added for Hand Detection
+    
+    def hand_callback(self, msg: Bool): # Entire method is added for Hand Detection
+        now = self.get_clock().now().nanoseconds * 1e-9 
+
+        if msg.data: 
+            self.hand_present = True 
+            self.last_true_time = now 
+        else:
+            # only release if false persists long enough 
+            if now - self.last_true_time > self.hold_time: 
+                self.hand_present = False 
+
 
     def loop(self):
+        if not self.hand_present: # Added for Hand Detection
+            return # Added for Hand Detection
         msg = Point()
         msg.x = 0.0
         msg.y = 0.0
